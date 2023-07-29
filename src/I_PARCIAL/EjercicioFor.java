831b122e6c4984b9e72dfc2b785dda5a3f5480c8
EduardoAjuchan <eduardoajuchan@gmail.com>
2023-07-29 17:29:04 -0600

    cronometro

---
+++ src/I_PARCIAL/EjercicioFor.java
@@ -0,0 +1,19 @@
+package I_PARCIAL;
+public class EjercicioFor {
+        public static void main(String[] args) {
+            for (int hora = 0; hora < 24; hora++) {
+                for (int minuto = 0; minuto < 60; minuto++) {
+                    for (int segundo = 0; segundo < 60; segundo++) {
+                        System.out.printf("%02d:%02d:%02d%n", hora, minuto, segundo);
+                        try {
+                            Thread.sleep(1000); // Pausa durante 1 segundo (1000 milisegundos)
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
---
+++ src/I_PARCIAL/Ejercicofor1.java
@@ -0,0 +1,21 @@
+package I_PARCIAL;
+
+public class Ejercicofor1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 80; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 1; i <= 24; i++) {
+            System.out.print("*");
+            for (int j = 1; j <= 78; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        for (int i = 1; i <= 80; i++) {
+            System.out.print("*");
+        }
+    }
+
+}
